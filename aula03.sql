8a4156cfdfc9e777f9e8033173d16ad608232296
Franciele Chesere <126211988+ChesereF@users.noreply.github.com>
2023-08-17 20:46:26 -0300

    Update aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,1 +1,28 @@
+CREATE DATABASE bd_aula03;
+\c bd_aula03;
 
+CREATE TABLE tbl_cliente (
+	codigo_cliente integer primary key,
+	Nome varchar (100) NOT NULL,
+	Cidade varchar (100),
+	Endereco varchar (100)
+);
+
+CREATE TABLE tbl_titulo (
+	codigo_titulo integer primary key,
+	titulo text NOT NULL,
+	descricao text,
+	categoria text
+);
+
+CREATE TABLE tbl_emprestimo (
+	numero_emprestimo integer primary key,
+	codigo_cliente varchar (20),
+	codigo_livro varchar (20)
+);
+
+CREATE TABLE tbl_livros (
+	cod_livro integer primary key,
+	codigo_titulo varchar (20),
+	status text 
+);
