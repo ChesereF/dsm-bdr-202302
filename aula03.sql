8a0079e6cfd80caee12c62a885628689abdf4f3e
Franciele Chesere <126211988+ChesereF@users.noreply.github.com>
2023-08-25 16:49:09 -0300

    Update aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE bd_aula03;
 \c bd_aula03;
 
+CREATE DOMAIN chk_categoria text CHECK (VALUE='DRAMA' OR VALUE='COMEDIA');
+CREATE DOMAIN chk_status text CHECK (VALUE='DISPONIVEL' OR VALUE='ALUGADO');
+
 CREATE TABLE tbl_cliente (
 	codigo_cliente integer primary key,
 	Nome varchar (100) NOT NULL,
@@ -12,22 +15,20 @@ CREATE TABLE tbl_titulo (
 	codigo_titulo integer primary key,
 	titulo text NOT NULL,
 	descricao text,
-	categoria text
+	categoria chk_categoria
 );
 
-CREATE TABLE tbl_emprestimo (
-	numero_emprestimo integer primary key,
-	codigo_cliente varchar (20),
-	codigo_livro varchar (20)
-);
 
 CREATE TABLE tbl_livros (
 	cod_livro integer primary key,
-	codigo_titulo varchar (20),
-	status text 
+	codigo_titulo integer REFERENCES tbl_titulo(codigo_titulo),
+	status chk_status 
 );
 
-CREATE DOMAIN chk_status text CHECK (VALUE='disponivel' OR VALUE='indisponivel')
 
-CREATE DOMAIN categoria  text
-CHECK (VALUE IN ('DRAMA', 'COOMEDIA'));
+CREATE TABLE tbl_emprestimo (
+	numero_emprestimo integer primary key,
+	codigo_cliente integer REFERENCES tbl_cliente(codigo_cliente),
+	codigo_livro integer REFERENCES tbl_livros(cod_livro)
+);
+
